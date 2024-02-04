2a32bade0303c9d0aea0b14796ffe93687e1bee3
nayon117 <hasibul.nayon1@gmail.com>
2024-02-04 09:25:18 +0600

    create pages layout

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>This is Contact</h2>
+    </div>
+  );
+};
+export default Contact;
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+
+
+const Projects = () => {
+  return (
+    <div>
+      <h2>This is Projects</h2>
+    </div>
+  );
+};
+export default Projects;
