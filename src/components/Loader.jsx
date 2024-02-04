cb13f0f72bc2ac461b22d9fdd7dde9009ddcaebe
nayon117 <hasibul.nayon1@gmail.com>
2024-02-04 09:48:04 +0600

    install react drei

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,16 @@
+import { Html } from "@react-three/drei";
 
 
 const Loader = () => {
-  return (
-    <div>
-      <h2>This is Loader</h2>
+    return (
+      <Html>
+             <div className="flex justify-center items-center">
+          <div className="w-20 h-20 border-opacity-20 border-2 border-blue-500 border-t-blue-500 rounded-full animate-spin">
+              
+     </div>
     </div>
+      </Html>
+   
   );
 };
 export default Loader;
