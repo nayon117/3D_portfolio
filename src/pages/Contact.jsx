2225735044ffd130bb6901b22d0c1760a03fd51f
nayon117 <hasibul.nayon1@gmail.com>
2024-02-05 09:27:08 +0600

    implement email js on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import  emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 
 const Contact = () => {
+  const formRef = useRef(null)
   const [form, setForm] = useState({ name: "", email: "", message: "" });
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SEVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+     {
+      from_name:form.name,
+      to_name:"Hasibul Hasan",
+      from_email:form.email,
+      to_email:"hasibul.nayon1@gmail.com",
+      message:form.message
+     },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(()=>{
+      setIsLoading(false);
+      // setForm({ name: "", email: "", message: "" });
+      // formRef.current.reset();
+    }).catch((error)=>{
+      setIsLoading(false);
+      console.log(error);
+    })
+  };
+
   const handleFocus = () => {};
   const handleBlur = () => {};
-  const handleSubmit = () => {};
 
   return (
     <section className="relative flex flex-col md:flex-row max-container">
